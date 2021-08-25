d47cecd2e1365f4f9e710c97c894973bac761faa
unknown <nasrullah_hussaini@yahoo.com>
2021-08-25 10:46:03 -0400

    create an action for redux

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const BUY_CAKE = "BUY_CAKE";
+
+//Action has two parts: a) An action creator which is a function and b )an action which is a method
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
