44b2a8e27a82825d5735780adc8afadc83725a8f
unknown <nasrullah_hussaini@yahoo.com>
2021-08-25 11:19:42 -0400

    add store to redux

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 //Action has two parts: a) An action creator which is a function and b )an action which is a method
@@ -27,3 +30,13 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated store", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
